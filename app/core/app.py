deed3c5f48a976317b00bc60aad779bdfabb84cf
Moshe Mizrachi <moshe@aka.co.il>
2022-05-30 17:10:15 +0300

    added user registraion and authentication

--- app/core/app.py
+++ app/core/app.py
@@ -1,10 +1,51 @@
-from fastapi import FastAPI
+from beanie import init_beanie
+from fastapi import Depends, FastAPI
 
-from .routes.user import router as user_router
+from .database import User, db
+from .user.schemas import UserCreate, UserRead, UserUpdate
+from .user.users import auth_backend, current_active_user, fastapi_users
 
 app = FastAPI()
 
-app.include_router(user_router, tags=["users"], prefix="/user")
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
+
+
+@app.on_event("startup")
+async def on_startup():
+    await init_beanie(
+        database=db,
+        document_models=[
+            User,
+        ],
+    )
 
 
 @app.get("/", tags=["Root"])
